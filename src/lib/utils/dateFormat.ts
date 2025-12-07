2ad8017d747f29a823a8919f124af6dce35c1566
Nguyen Van Thai <thaiblog116@gmail.com>
2025-12-07 15:19:57 +0700

    update format date

--- src/lib/utils/dateFormat.ts
+++ src/lib/utils/dateFormat.ts
@@ -1,11 +1,12 @@
 import { format } from "date-fns";
+import { vi } from "date-fns/locale";
 
 const dateFormat = (
   date: Date | string,
-  pattern: string = "dd MMM, yyyy",
+  pattern: string = "dd MMMM, yyyy"
 ): string => {
   const dateObj = new Date(date);
-  const output = format(dateObj, pattern);
+  const output = format(dateObj, pattern, { locale: vi });
   return output;
 };
 
